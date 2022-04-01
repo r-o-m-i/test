127471a1295611630f9ed7f62c7409345668b4e3
Rohit Goparaju <rohitgoparaju.r96@gmail.com>
2022-04-01 22:08:36 +0530

    first commit

---
+++ src/main/java/com/in28minutes/App.java
@@ -0,0 +1,10 @@
+package com.in28minutes;
+
+public class App {
+
+	public static String returnSomething()
+	{
+		return "DUMMY";
+	}
+	
+}
---
+++ src/test/java/com/in28minutes/AppTest.java
@@ -0,0 +1,14 @@
+package com.in28minutes;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void test() {
+		assertEquals("DUMMY", App.returnSomething());
+	}
+
+}
